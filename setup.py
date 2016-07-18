31ffef3bc8b15a505c37fdb7d35ae1d7ff65d9c8
Richard C. Nason <rnason@atlantean.io>
2016-07-18 19:59:56 -0400

    Updated manifest to fix pypi upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """Setup file to package mkdocs-inspired theme"""
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 
 setup(
@@ -9,14 +9,15 @@ setup(
     version=VERSION,
     url='https://github.com/rnason/mkdocs-inspired.git',
     license='MIT',
-    description='Mobile responsive Mkdocs theme with slider Nav based on bootswatch flatly',
+    description='Mobile responsive Mkdocs theme with slider nav based on bootswatch flatly',
     author='Richard Nason',
     author_email='rich@nason.co',
     packages=find_packages(),
+    keywords="pip package, mkdocs, themes",
     include_package_data=True,
     entry_points={
         'mkdocs.themes': [
-            'inspired = mkdocs-inspired',
+            'inspired = mkdocs_inspired',
         ]
     },
     zip_safe=False
